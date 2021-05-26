803c08e23dd7a5fa4c3b57c7a5ec441c0bdddbd6
Juan Carlos Choque Quispe <juan.c.c.q@gmail.com>
2021-05-26 19:30:58 -0400

    added controller for items

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3006;
 const homeController = require("./controllers/homeController");
 const inventoryController = require("./controllers/inventoryController");
+const itemController = require("./controllers/itemController");
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -22,6 +23,7 @@ app.delete("/", (req, res) => {
 
 app.get("/home", homeController.index);
 app.get("/inventory", inventoryController.index);
+app.get("/item", itemController.index);
 
 app.listen(port, () => {
   console.log("project started on port 3006");
