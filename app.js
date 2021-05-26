0e05a9c068b474b5e25ee1179f0218379de4f891
Juan Carlos Choque Quispe <juan.c.c.q@gmail.com>
2021-05-26 15:08:04 -0400

    added routes for application

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.post("/", (req, res) => {
+  res.send("Got a post request");
+});
+
+app.put("/", (req, res) => {
+  res.send("Got a put request");
+});
+
+app.delete("/", (req, res) => {
+  res.send("Got a delete request");
+});
+
 app.listen(port, () => {
   console.log("project started");
 });
